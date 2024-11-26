f61dad7bce91a1b8f6090e2a30025724c5ee3b48
gourabofficial <gangulygourab3@gmail.com>
2024-11-26 12:50:09 +0530

    do while loop

--- Loops/tea.js
+++ Loops/tea.js
@@ -2,12 +2,12 @@ const input = require('readline-sync');
 let teacollection = [];
 let userinput;
 do {
-   userinput = input.question(`Enter your favourite tea: (type "Stop" to finish )`)
-  if(userinput !== "Stop")
-  teacollection.push(userinput);
-} while (userinput !== "Stop") {
-  console.log("You Leave")
-}
+  userinput = input.question(`Enter your favourite tea: (type "Stop" to finish )`);
+  if (userinput !== "Stop")
+    teacollection.push(userinput);
+} while (userinput !== "Stop")
+
+console.log("You Leave")
 console.log(teacollection);
 
 
